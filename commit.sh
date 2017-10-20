4dd054cb504a9ca7df3985602bb3d55dbaae394e
czaraugust <cesaraugusto@ic.ufal.br>
2017-10-20 18:09:35 -0300

    Improve script

--- commit.sh
+++ commit.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 read -p "Commit description: " desc
 git status &&\
+echo "Aperte <ENTER> para ADD"
+read ' ' 
 git add . && \
 git add -u && \
-git status &&\
+echo "Aperte <ENTER> para COMMIT"
+read ' ' 
 git commit -m "$desc" && \
+echo "Aperte <ENTER> para PUSH"
+read ' ' 
 git push origin master
-git status &&\
+
